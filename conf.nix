29853af67570da392dd2e409302533375170c771
Robin Stumm <serverkorken@gmail.com>
2020-03-26 12:18:43 +0100

    social buttons

--- conf.nix
+++ conf.nix
@@ -3,10 +3,19 @@
 
   theme = {
     itemsPerPage = 10;
+
     site = {
       title = "dermetfan";
       description = "Blog";
       copyright = "Public Domain";
+    };
+
+    social = {
+      github = https://github.com/dermetfan;
+      # email = "serverkorken@gmail.com"; # bug in styx templates/partials/content-pre.nix
+      linked-in = https://www.linkedin.com/in/dermetfan;
+      stack-overflow = https://stackoverflow.com/users/2317275/dermetfan?tab=profile;
+      twitter = https://twitter.com/dermetfan;
     };
 
     lib.highlightjs = {
--- data/pages/about.md
+++ data/pages/about.md
@@ -11,12 +11,6 @@ Prior work worth mentioning:
 - [libGDX] tutorials on [YouTube](https://youtube.com/dermetfan)
 - [`libgdx-utils`](https://bitbucket.org/dermetfan/libgdx-utils) and `libgdx-utils-box2d`, a support library for [libGDX]-powered games and applications
 
-Find me on:
-
-- [Bitbucket](https://bitbucket.org/dermetfan/)
-- [GitHub](https://github.com/dermetfan)
-- [StackOverflow](https://stackoverflow.com/users/2317275/dermetfan?tab=profile)
-
 [Rust]: https://rust-lang.org
 [NixOS]: https://nixos.org
 [libGDX]: https://libgdx.com
