5d27045935ef10fa4d975b32c217382bb32f1fb1
Robin Stumm <serverkorken@gmail.com>
2020-04-21 19:35:34 +0200

    add workaround style for https://github.com/asciidoctor/asciidoctor/issues/1087

--- themes/dermetfan/templates/partials/head/css-extra.nix
+++ themes/dermetfan/templates/partials/head/css-extra.nix
@@ -1,4 +1,14 @@
-{ templates, ... }:
+{ lib, templates, ... }:
 data:
 
-templates.tag.link-css { href = templates.url "/styles/asciidoctor-default-anchor.css"; }
+lib.concatStrings [
+  (templates.tag.link-css { href = templates.url "/styles/asciidoctor-default-anchor.css"; })
+  ''
+    <style>
+    .olist > ol > li > p,
+    .ulist > ul > li > p {
+        margin-bottom: 0;
+    }
+    </style>
+  ''
+]
